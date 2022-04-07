bdde5ff1ba5bb0fdc8d1dbf749a5b877f94a4aa7
Artem <jaksan37@gmail.com>
2022-04-07 13:18:38 +0300

    first homework

---
+++ src/Components/CompFunc/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = (props) => {
+  return (
+    <button className="form-btn" onClick={props.click}>
+      Добавить
+    </button>
+  );
+};
---
+++ src/Components/CompFunc/Form.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Input } from "./Input";
+
+export const Form = () => {
+  const [name, setName] = useState("click");
+  const [value, setValue] = useState("");
+  const [toDolist, setToDoList] = useState([]);
+
+  const handleClick = () => {
+    if (value === "") {
+      alert("Вы должны что то написать!");
+      return false;
+    } else {
+      setToDoList([...toDolist, value]);
+      setValue("");
+    }
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const closeItem = (event) => {
+    event.target.parentElement.style.display = "none";
+  };
+
+  const checkedItem = (event) => {
+    event.target.classList.toggle("checked");
+  };
+
+  return (
+    <>
+      <div className="main-form">
+        <div className="form-title">
+          <h1 className="toDo-title">Список дел</h1>
+          <div className="form-wrp">
+            <Input change={handleChange} value={value} />
+            <Button name={name} click={handleClick} />
+          </div>
+        </div>
+        <div className="toDo-basket">
+          <ul className="toDo-list">
+            {toDolist.map((toDolist) => (
+              <li onClick={checkedItem} className="toDo-item">
+                {toDolist}
+                <span className="close" onClick={closeItem}>
+                  &#215;
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};

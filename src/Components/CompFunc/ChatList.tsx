8c066c31dfe5cb53f9faeaa796d1a4559a37ddfc
Artem <jaksan37@gmail.com>
2022-04-23 20:43:04 +0300

    fix

--- src/Components/CompFunc/ChatList.tsx
+++ src/Components/CompFunc/ChatList.tsx
@@ -24,12 +24,12 @@ export const ChatList: FC<ChatListProps> = ({ chatList, onAddChat }) => {
   };
 
   return (
-    <div className="chatlist">
-      <ul className="chatlist-list">
+    <div className="chat">
+      <ul className="chat-list">
         {chatList.map((chat) => (
-          <li className="chatlist-list-item" key={chat.id}>
+          <li className="chat-list-item" key={chat.id}>
             <NavLink
-              className="chatlist-list-links"
+              className="chat-list-links"
               to={`/chats/${chat.name}`}
               style={({ isActive }) => ({
                 background: isActive ? 'rgba(255, 255, 255, 0.1)' : 'none',
@@ -40,15 +40,15 @@ export const ChatList: FC<ChatListProps> = ({ chatList, onAddChat }) => {
           </li>
         ))}
       </ul>
-      <h1 className="chatlist-title">chatlist</h1>
-      <form className="chatlist-form" onSubmit={handleSubmit}>
+      <h1 className="chat-title">chatlist</h1>
+      <form className="chat-form" onSubmit={handleSubmit}>
         <input
-          className="chatlist-form-input"
+          className="chat-form-input"
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <button disabled={!name} className="chatlist-form-btn" type="submit">
+        <button disabled={!name} className="chat-form-btn" type="submit">
           Add chat
         </button>
       </form>
